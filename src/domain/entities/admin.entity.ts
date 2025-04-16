d6d0202ba90eab3a0d3316707118528bcc2b56f2
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2025-04-16 10:11:00 -0500

    create new entity

---
+++ src/domain/entities/admin.entity.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Index,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Admin {
+  @PrimaryGeneratedColumn({ type: "bigint", unsigned: true })
+  id!: number;
+
+  @Column({ type: "varchar", length: 55, nullable: true })
+  AdminName!: string | null;
+
+  @Index({ unique: true })
+  @Column({ type: "varchar", length: 255 })
+  email!: string;
+
+  @Column({ nullable: false })
+  password!: string;
+
+  @Index()
+  @Column({ nullable: true, type: "varchar" })
+  uuid!: string | null;
+
+  @Column({
+    type: "varchar",
+    length: 50,
+    default: "admin",
+    nullable: false,
+  })
+  role!: string;
+
+  @CreateDateColumn({
+    precision: 0,
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  createAt!: Date;
+
+  @UpdateDateColumn({
+    precision: 0,
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+    onUpdate: "CURRENT_TIMESTAMP",
+  })
+  lastUpdate!: Date;
+}
