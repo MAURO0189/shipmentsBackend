38c95c71380a4f7ccf675a175acc48ffad9053e7
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2025-04-16 07:00:13 -0500

    Primer commit - reto tecnico

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
