c84ebb74091391de2061cacfcb3fc141e4c61ad4
MAURO0189 <108503288+MAURO0189@users.noreply.github.com>
2025-04-17 06:44:00 -0500

    add new route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,58 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { adminRouter } from "./infrastructure/routes/admin.route";
+import { createClient } from "redis";
+import { AppDataSource } from "./infrastructure/database/data-source";
 
 dotenv.config();
+
+const allowdHost = ["http://localhost:3000", "https://localhost:3000"];
+
+const devHosts = [
+  "http://localhost:3000",
+  "http://localhost:4200",
+  "http://127.0.0.1:4200",
+  "http://127.0.0.1:5500",
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+//Routes
+app.use("/api/admin", adminRouter);
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+app.use(
+  cors({
+    origin: process.env.NODE_ENV === "production" ? allowdHost : devHosts,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+export const redisClient = createClient();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  const startTime = new Date().toLocaleString();
+  console.log(`🚀 Server started at ${startTime}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.on("error", (err) => {
+  console.error(`❌ Server error:${err}`);
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Data Source initialized!");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Error initializing Data Source", err);
+  });
